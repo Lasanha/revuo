bd116be6bd297425a9cac2c2a58803e903c4a743
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-01-21 18:45:58 -0200

    tests for news, media, publications, blog, staff views

--- revuo/tests.py
+++ revuo/tests.py
@@ -19,3 +19,47 @@ class PortalTest(LiveServerTestCase):
         """
         self.browser.get(self.live_server_url + '/')
         self.assertIn('Home', self.browser.title)
+
+
+    def test_news_page(self):
+        """
+        news page test at /news
+        """
+        self.browser.get(self.live_server_url + '/news')
+        self.assertIn('News', self.browser.title)
+
+
+    def test_media_page(self):
+        """
+        news page test at /media
+        """
+        self.browser.get(self.live_server_url + '/media')
+        self.assertIn('Media', self.browser.title)
+
+
+    def test_publications_page(self):
+        """
+        news page test at /publications
+        """
+        self.browser.get(self.live_server_url + '/publications')
+        self.assertIn('Publications', self.browser.title)
+
+
+    def test_blog_page(self):
+        """
+        news page test at /blog
+        """
+        self.browser.get(self.live_server_url + '/blog')
+        self.assertIn('Blog', self.browser.title)
+
+
+    def test_staff_page(self):
+        """
+        news page test at /staff
+        """
+        self.browser.get(self.live_server_url + '/staff')
+        self.assertIn('Staff', self.browser.title)
+
+
+
+
