24cd0e613cbdcab5b3ad92441b16deb2de4a2a1c
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-01-21 20:47:19 -0200

    models editor and admin

--- revuo/models.py
+++ revuo/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Author(models.Model):
+    user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return ' '.join([self.user.first_name, self.user.last_name])
+
+
+class Editor(models.Model):
+    user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return ' '.join([self.user.first_name, self.user.last_name])
+
+
+class Admin(models.Model):
     user = models.OneToOneField(User)
 
     def __unicode__(self):
