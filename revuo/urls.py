7456c991a1faad16d9fbabfd91dcd94fcbe34160
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-01-21 18:57:20 -0200

    basic empty views for more pages (news, publications, blog, staff, media)

--- revuo/urls.py
+++ revuo/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from views import Home
+from views import Home, News, Media, Publications, Blog, Staff
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view()),
+    url(r'^news$', News.as_view()),
+    url(r'^media$', Media.as_view()),
+    url(r'^publications$', Publications.as_view()),
+    url(r'^blog$', Blog.as_view()),
+    url(r'^staff$', Staff.as_view()),
 )
--- revuo/views.py
+++ revuo/views.py
@@ -6,3 +6,38 @@ class Home(View):
 
     def get(self, request):
         return render(request, self.template_name, {})
+
+
+class News(View):
+    template_name = 'revuo/news.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+
+class Media(View):
+    template_name = 'revuo/media.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+
+class Publications(View):
+    template_name = 'revuo/publications.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+
+class Staff(View):
+    template_name = 'revuo/staff.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+
+class Blog(View):
+    template_name = 'revuo/blog.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
