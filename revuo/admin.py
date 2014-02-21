4f323842c05ce62500386988d50dfbf54cf04999
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-02-20 22:24:57 -0300

    making imports explicit

--- revuo/admin.py
+++ revuo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Author, NewsItem, BlogItem, VideoItem
+from revuo.models import Author, NewsItem, BlogItem, VideoItem
 
 admin.site.register(Author)
 admin.site.register(NewsItem)
