460b0b042d7b9e85c99328620f201a1521aa175d
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-02-20 22:51:07 -0300

    adding django-pdb into requirements, installed apps, middleware classes

--- portal/settings.py
+++ portal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_pdb',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -46,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_pdb.middleware.PdbMiddleware',
 )
 
 ROOT_URLCONF = 'portal.urls'
