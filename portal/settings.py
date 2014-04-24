000ee2b945a7c132321e135b380daa199a199f54
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-04-24 18:19:44 -0300

    loading secret key from env

--- portal/settings.py
+++ portal/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wt8b7)pm1u6&@fp%tlrpwb20g3azk6t9_4&xz8ejk!!$q6(g*5'
+SECRET_KEY = os.environ['REVUO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
