9e56f365d69c4dd4c8ecbe383a71ffcf0e0608c1
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-04-24 18:49:33 -0300

    defining debug and prod database settings

--- portal/settings.py
+++ portal/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if DEBUG:
+    DATABASES = {'default': {'ENGINE':'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
+else:
+    db_url = os.environ['DATABASE_URL']
+    DATABASES = {'default': dj_database_url.config(default=db_url)}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
