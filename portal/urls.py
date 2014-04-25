30002c307300f3acfcf66ee4f6062697ee1965f5
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-04-25 19:40:59 -0300

    url for static serving on production

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from portal import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +10,8 @@ urlpatterns = patterns('',
     url(r'', include(revuo_patterns)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        )
