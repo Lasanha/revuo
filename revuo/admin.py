08d3bef45821f9a20876a7f4765445565231e800
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-04-25 23:32:19 -0300

    adding Publication on admin

--- revuo/admin.py
+++ revuo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from revuo.models import Author, NewsItem, BlogItem, VideoItem
+from revuo.models import Author, NewsItem, BlogItem, VideoItem, Publication
 
 admin.site.register(Author)
 admin.site.register(NewsItem)
 admin.site.register(VideoItem)
 admin.site.register(BlogItem)
+admin.site.register(Publication)
