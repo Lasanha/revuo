a80301200d517c2ca0793f888f214c925e89a269
Gabriel Marcondes <gabrielgeraldo@gmail.com>
2014-04-26 00:23:15 -0300

    defining widget for publication description

--- revuo/forms.py
+++ revuo/forms.py
@@ -22,7 +22,7 @@ class FormBlogItem(forms.ModelForm):
 
 class FormPublication(forms.Form):
    title = forms.CharField(max_length=140)
-   description = forms.CharField(max_length=280)
+   description = forms.CharField(max_length=280, widget=forms.Textarea)
    attachment = forms.FileField()
 
 
